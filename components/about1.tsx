c6f430efaf1560bef540f5d90eb336eddca3d5d4
redletterdays <shafisaleh@gmail.com>
2025-02-17 12:00:45 +0000

    added px-17 to pricing spans on about section for padding inline to align them with all the ticks

--- components/about1.tsx
+++ components/about1.tsx
@@ -49,7 +49,7 @@ const About1 = () => {
                   Strategic Meeting to Provide Recommendations for Your Business
                   Plan & Financial Forecast Model
                 </ListTick>
-                <span className="text-md font-semibold font-open-sans">
+                <span className="px-17 text-md font-semibold font-open-sans">
                   495 GBP
                 </span>
               </div>
@@ -105,8 +105,8 @@ const About1 = () => {
                   Strategic Meeting to Provide Recommendations for Your Business
                   Plan & Financial Forecast Model
                 </ListTick>
-                <span className="text-md font-semibold font-open-sans">
-                  495 GBP
+                <span className="px-17 text-md font-semibold font-open-sans">
+                  795 GBP
                 </span>
               </div>
             </div>
@@ -122,21 +122,18 @@ const About1 = () => {
                   Full Expedition
                 </span>
                 <h2 className="mb-5 text-3xl font-bold text-dark sm:text-[40px]/[48px]">
-                  You’ve done the legwork of writing the business plan
+                  Planning to start a business but don't have the time or the
+                  desire to handle the online research and preparation?
                 </h2>
                 <p className="mb-5 text-base text-body-color font-open-sans">
-                  You did some research and started to build the financial
-                  model, but if you’re unsure if you’re on the right track,
-                  we're here to guide you.
+                  We’ll handle we’ll handle everything — from creating your
+                  Business Plan and Financial Forecast Model to conducting
+                  in-depth online research with our in-house research team.
                 </p>
                 <p className="mb-8 text-base text-body-color font-open-sans">
-                  We can help with refining your Business Plan and Financial
-                  Forecast Model so you can move forward with confidence.
-                </p>
-                <p className="mb-8 text-base text-body-color font-open-sans">
-                  We will have a Strategic Meeting to thoroughly review your
-                  Business Plan, Financial Forecast Model and provide
-                  recommendations.
+                  You’ll only need to handle primary research because as a
+                  business owner, connecting with your customers is vital for
+                  your business success and shaping your vision.
                 </p>
                 <a
                   href="javascript:void(0)"
@@ -148,18 +145,23 @@ const About1 = () => {
             </div>
             <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
               <div className="mt-10 lg:mt-0 space-y-4">
-                <ListTick>Refining Your Business Plan</ListTick>
-                <ListTick>Reviewing Your Financial Forecast Model</ListTick>
+                <ListTick>Writing Your Business Plan</ListTick>
+                <ListTick>Creating Your Financial Forecast Model</ListTick>
+                <ListTick>
+                  In-House Team Will Conduct Your Secondary Research In-Depth
+                </ListTick>
                 <ListTick>
                   Personalised Step by Step Guide To Moving Forward with Your
                   Business Plan
                 </ListTick>
+                <ListTick>Research Review Meeting</ListTick>
+                <ListTick>Designing Your Primary Research Questions</ListTick>
                 <ListTick>
                   Strategic Meeting to Provide Recommendations for Your Business
                   Plan & Financial Forecast Model
                 </ListTick>
-                <span className="text-md font-semibold font-open-sans">
-                  795 GBP
+                <span className="px-17 text-md font-semibold font-open-sans">
+                  1,195 GBP
                 </span>
               </div>
             </div>
