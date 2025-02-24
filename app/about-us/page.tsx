f0ab256c4a386c4bfb42321d4d8515d588ef9f4f
redletterdays <shafisaleh@gmail.com>
2025-02-24 20:08:54 +0000

    added Team component and imported into about us route, removed dark mode tailwind classes

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "Team | Hampshire Business Plans",
+  title: "About Us | Hampshire Business Plans",
   description:
     "Hampshire Business Plans offers tailored business planning and financial forecasting services for entrepreneurs. Get expert guidance and support to launch and grow your business.",
 };
@@ -7,14 +7,15 @@ export const metadata = {
 import Cta from "@/components/cta";
 import Faq from "@/components/faq";
 import Banner from "@/components/banner";
-import Privacy from "@/components/privacy";
 import AboutUs from "@/components/about-us";
+import Team from "@/components/team";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <AboutUs />
+      <Team />
       <Cta />
       <Faq />
     </>
